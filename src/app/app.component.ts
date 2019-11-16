4642e6cb2062e3baab89c0ef491f5d6a86c53997
aojoray <seonyoung.mun@student.uts.edu.au>
2019-11-16 15:01:37 +1100

    Added countries to history list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'countries-app';
+  histories = [];
+  searchCountry(country) {
+    this.histories.push(country);
+    console.log(this.histories);
+  }
+
 }
